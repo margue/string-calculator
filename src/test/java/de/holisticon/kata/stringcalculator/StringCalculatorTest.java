d805c2f7167b7854f189cb3ebb190aea2dab2185
Martin Guenther <martin.guenther@bdk-bank.de>
2017-12-11 16:14:37 +0100

    test: add first test for empty string, no implementation yet

--- src/test/java/de/holisticon/kata/stringcalculator/StringCalculatorTest.java
+++ src/test/java/de/holisticon/kata/stringcalculator/StringCalculatorTest.java
@@ -1,4 +1,20 @@
 package de.holisticon.kata.stringcalculator;
 
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
 public class StringCalculatorTest {
+
+  @Test
+  public void shouldAddEmptyString(){
+
+    int result = add("");
+
+    Assertions.assertThat(result).isEqualTo(0);
+  }
+
+  private int add(String numbers) {
+    throw new UnsupportedOperationException("not yet implemented");
+  }
+
 }
