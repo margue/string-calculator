b76b909d403707061171d3dda1a45b22244cf02e
Martin Guenther <martin.guenther@bdk-bank.de>
2017-12-11 16:20:23 +0100

    feat: implement add for single number string using value of numbers

--- src/test/java/de/holisticon/kata/stringcalculator/StringCalculatorTest.java
+++ src/test/java/de/holisticon/kata/stringcalculator/StringCalculatorTest.java
@@ -16,18 +16,26 @@ public class StringCalculatorTest {
   }
 
   @Test
-  public void shouldAddSingleNumberString(){
+  public void shouldAddSingleNumberString_1(){
 
     int result = add("1");
 
     assertThat(result).isEqualTo(1);
   }
 
+  @Test
+  public void shouldAddSingleNumberString_5(){
+
+    int result = add("5");
+
+    assertThat(result).isEqualTo(5);
+  }
+
   private int add(String numbers) {
     if(numbers.isEmpty()) {
       return 0;
     } else {
-      return 1;
+      return Integer.valueOf(numbers);
     }
   }
 
