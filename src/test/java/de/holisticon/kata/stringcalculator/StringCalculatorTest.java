3f2e3cda07bb246bd0e33906f5785a868252d68e
Martin Guenther <martin.guenther@bdk-bank.de>
2017-12-11 16:31:35 +0100

    feat: add shouldAddStringWithThreeNumbers

--- src/test/java/de/holisticon/kata/stringcalculator/StringCalculatorTest.java
+++ src/test/java/de/holisticon/kata/stringcalculator/StringCalculatorTest.java
@@ -39,12 +39,23 @@ public class StringCalculatorTest {
     assertThat(result).isEqualTo(7);
   }
 
+  @Test
+  public void shouldAddStringWithThreeNumbers(){
+    int result = add("3,1,7");
+
+    assertThat(result).isEqualTo(11);
+  }
+
   private int add(String numbers) {
     if(numbers.isEmpty()) {
       return 0;
     } if(numbers.contains(",")) {
       String[] split = numbers.split(",");
-      return Integer.valueOf(split[0]) + Integer.valueOf(split[1]);
+      int sum = 0;
+      for (int i = 0; i < split.length; i++) {
+        sum += Integer.valueOf(split[i]);
+      }
+      return sum;
     } else {
       return Integer.valueOf(numbers);
     }
